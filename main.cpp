47540df283a4ca1bc9eb57dc2862d72f9323742e
Joaquim Juneo <jjsoprofissional@gmail.com>
2026-09-09 23:38:02 -0300

    leitura dos filmes funcional, leitura dos cinemas não funcional

--- main.cpp
+++ main.cpp
@@ -4,32 +4,37 @@
 #include<fstream>
 #include <chrono>
 #include <vector>
+#include <sstream>
+#include <cassert>
+
+// #include<classe.cpp>
+
+using namespace std;
 
 
 
-class InsertionSort
+
+
+class BuscaBinaria
 {
 private:
     /* data */
 public:
-    InsertionSort(/* args */);
-    ~InsertionSort();
-    static void Sort(Filme * filmes){
-        
-    }
+    BuscaBinaria(/* args */);
+    ~BuscaBinaria();
+    // static int BuscaFilme(<vector> Filme){
+
+    // }
 };
 
-InsertionSort::InsertionSort(/* args */)
+BuscaBinaria::BuscaBinaria(/* args */)
 {
 }
 
-InsertionSort::~InsertionSort()
+BuscaBinaria::~BuscaBinaria()
 {
 }
 
-
-
-using namespace std;
 
 class Filme
 {
@@ -44,39 +49,134 @@ public:
     int duracao;
     std::string genero;
 };
+class Cinema{
+public:
+    std::string ID;
+    std::string nome;
+    double coord_X;
+    double coord_Y;
+    double preco;
+    vector <string> filmes; 
+};
 
 
 
 int main(int argc, char const *argv[])
 {
+    // assert(0);
     // Abre o arquivo para leitura
-    ifstream arquivo("filmesCrop.txt");
+    ifstream arq_filmes("filmesCrop.txt");
+    ifstream arq_cinemas("cinemas.txt");
+
+    vector <Filme> filmes; 
+    vector <Cinema> cinemas; 
     
     // Verifica se o arquivo abriu corretamente
-    if (!arquivo.is_open()) {
-        cout << "Erro ao abrir o arquivo!" << endl;
+    if (!arq_filmes.is_open()) {
+        cout << "Erro ao abrir o arquivo de filmes!" << endl;
+        return 1;
+    }
+    if (!arq_cinemas.is_open()) {
+        cout << "Erro ao abrir o arquivo de cinemas" << endl;
         return 1;
     }
     
     string linha;
-    int qtdlinha = 0;
+    int qtdlinha = 0, linhaini = 1;
     // Registra o tempo inicial
-    auto inicio = std::chrono::high_resolution_clock::now();
+    auto ini_read_filmes = std::chrono::high_resolution_clock::now();
     
-    // Lê o arquivo linha por linha
-    while (qtdlinha <= 100000) {
-        getline(arquivo, linha);
+    // Lê o arquivo de filmes linha por linha
+    while (
+        qtdlinha <= 10 &&
+        getline(arq_filmes, linha)) {
+        if(linhaini == 1) {
+            linhaini++;
+            continue;
+        }
+
+
+        stringstream ss(linha);
+        string campo;
+        vector<string> campos;
+
+        while (getline(ss, campo, '\t')) {
+            campos.push_back(campo); // lÊ todos os campos da linha de filme
+        }
+
+        if (campos.size() < 9) continue;
+        // for(int a = 0; a<9;a++){assert(campos[a]);}
+
+        //atriui os trem lido a um filme, e coloca no array
+        Filme f;
+        f.identificacao     = campos[0];
+        f.tipo_do_filme     = campos[1];
+        f.titulo_primario   = campos[2];
+        f.titulo_original   = campos[3];
+        f.adulto            = (campos[4] == "1");
+        // assert(0);
+        f.ano_estreia       = (campos[5] == "\\N") ? 0 : stoi(campos[5]); // se foe \n vira 0
+        f.ano_fim           = (campos[6] == "\\N") ? 0 : stoi(campos[6]);
+        f.duracao           = (campos[7] == "\\N") ? 0 : stoi(campos[7]);
+        f.genero            = campos[8];
+
+        filmes.push_back(f);
+
         cout << linha << endl;
         qtdlinha++;
+
     }
-    
     // Registra o tempo final
-    auto fim = std::chrono::high_resolution_clock::now();
+    auto fim_read_filmes = std::chrono::high_resolution_clock::now();
+    qtdlinha = 0;
+    linhaini = 0;
+    auto ini_read_cinemas = std::chrono::high_resolution_clock::now();
+    // le o arquivo de cinemas linha por linha
+    while (getline(arq_cinemas, linha))
+    {
+        if(linhaini == 1) { // pula o cabeçalho
+            linhaini++;
+            continue;
+        }
+        
+        stringstream ss(linha);
+        string campo;
+        vector<string> campos;
+        
+        while (getline(ss, campo, ',')) {
+            campos.push_back(campo); // lÊ todos os campos da linha de cinema
+        }
+        
+        // if (campos.size() < 6) continue;
+        Cinema c;
+        c.ID = campos[0];
+        // assert(0);
+        c.nome = campos[1];
+        c.coord_X = (campos[2] == "\\N") ? 0 : stod(campos[2]);
+        assert(0);
+        c.coord_Y = (campos[3] == "\\N") ? 0 : stod(campos[3]);
+        c.preco = (campos[4] == "\\N") ? 0 : stod(campos[4]);
+        c.filmes.push_back(campos[5]);
+        c.filmes.push_back(campos[6]);
+        c.filmes.push_back(campos[7]);
+
+        cinemas.push_back(c);
+
+        std::cout << linha << endl;
+        qtdlinha++;
+    }
+    auto fim_read_cinemas = std::chrono::high_resolution_clock::now();
+    
+    
+    
 
     // Calcula a duração subtraindo o início do fim
-    std::chrono::duration<double, std::milli> duracao = (fim - inicio) ;
+    std::chrono::duration<double, std::milli> duracao_read_filmes = (fim_read_filmes - ini_read_filmes) ;
+    std::chrono::duration<double, std::milli> duracao_read_cinemas = (fim_read_cinemas - ini_read_cinemas) ;
 
-    std::cout << "Tempo de execução: " << duracao.count() << " ms" << std::endl;
+    cout << "---------------------------------------------------------------------------------------------------------------------------" << endl;
+    std::cout << "Leitura filmes: " << duracao_read_filmes.count() << " ms " << "Filmes lidos: "<< filmes.size()<< std::endl;
+    std::cout << "Leitura cinemas: " << duracao_read_cinemas.count() << " ms " << "Cinemas lidos: "<<cinemas.size()<< std::endl;
 
 
     // Fecha o arquivo
